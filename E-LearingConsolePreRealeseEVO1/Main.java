91c6e0571a9732b6892ea7b1ba95b69f99e8c7ac
Antonio Lanuto <39790759+antoniolanuto2001@users.noreply.github.com>
2021-12-20 14:59:53 +0100

    Add files via upload
    Creazione Di Supporto per Domande a Risposta Multipla

--- E-LearingConsolePreRealeseEVO1/Main.java
+++ E-LearingConsolePreRealeseEVO1/Main.java
@@ -17,10 +17,9 @@ public class Main {
 		int numeroDiquizDacreare;
 		String risposteBufferString;
 		String DomandeBufferString;
-		String SvuotaBuffer;
+		String selezioneModalitaString;
 		Scanner readScanner= new Scanner(System.in);
-		
-		
+				
 		System.out.println("Ho fatto Accesso con utente : "+antonioUtente.getNome());
 		System.out.println("Informazioni : ");
 		System.out.println("Nome           : "+antonioUtente.getNome());
@@ -29,14 +28,16 @@ public class Main {
 		System.out.println("Data           : "+antonioUtente.getDatadinascita().getGiorno()+"/"+antonioUtente.getDatadinascita().getMese()+"/"+antonioUtente.getDatadinascita().getAnno());
 		System.out.println("Sistema di Configurazione Domande Primitivo EVO 1.1");
 		
+		//Sistema_Configurazione_Manuale
+		/*
 		provaQuiz.AddParametri(12, 0, "A");
 		provaQuiz.AggiungiDomanda(nuovaDomanda);
 		provaTest.AddQuiz(provaQuiz);
-		
 		System.out.println("Test           : "+provaTest.getNomeTest());
 		System.out.println("Domanda: "+provaTest.QuizPresenti.get(0).Domande.get(0).Domanda);
 		risposteBufferString=inputScanner.next();
 		System.out.println("Risposta: "+provaTest.QuizPresenti.get(0).Domande.get(0).Riposta);		
+		*/
 		
 		//Prova_in_maniera_dinamica
 		System.out.println("Inizio procedura Dinamica ...");
@@ -49,23 +50,67 @@ public class Main {
 			int punteggiopositvo=inputScanner.nextInt();
 			System.out.print("Punteggio Negativo: ");
 			int negativopositvo=inputScanner.nextInt();
-			testdinamicoTest.QuizPresenti.get(i).AddParametri(punteggiopositvo, negativopositvo, "A");
-			System.out.print("Domanda: ");
-			DomandeBufferString=readScanner.nextLine();
-			System.out.print("Risposta: ");
-			risposteBufferString=readScanner.nextLine();
-			testdinamicoTest.QuizPresenti.get(i).AggiungiDomanda(new Domanda(DomandeBufferString, risposteBufferString));	
-			
-		}
-		System.out.println("Ho finito correttamente");
-		System.out.println("Ora Domande \n \n");
-		for (int i = 0; i < numeroDiquizDacreare; i++) 
-		{
-			System.out.println("Domanda - "+testdinamicoTest.QuizPresenti.get(i).Domande.get(0).Domanda);
-			risposteBufferString=readScanner.nextLine();
-			System.out.println("Risposta- "+testdinamicoTest.QuizPresenti.get(i).Domande.get(0).Riposta);
+			System.out.print("Seleziona Modalita Domanda : ");
+			selezioneModalitaString=inputScanner.next();
+			selezioneModalitaString.toUpperCase();
+			if (selezioneModalitaString.contentEquals("M")) 
+			{
+				testdinamicoTest.QuizPresenti.get(i).AddParametri(punteggiopositvo, negativopositvo, "M");
+				System.out.print("Inserire il numero di domande");
+				int numerodomande=inputScanner.nextInt();
+				System.out.println("Domanda Principale - ");
+				System.out.print("Domanda: ");
+				DomandeBufferString=readScanner.nextLine();
+				System.out.print("Risposta: ");
+				risposteBufferString=readScanner.nextLine();
+				testdinamicoTest.QuizPresenti.get(i).AggiungiDomanda(new Domanda(DomandeBufferString, risposteBufferString));
+				char lettera='A';
+				for (int j = 0; j < numerodomande; j++) 
+				{
+					System.out.print("Domanda"+lettera+"- ");
+					DomandeBufferString=Character.toString(lettera)+"- "+readScanner.nextLine();
+					risposteBufferString="";
+					testdinamicoTest.QuizPresenti.get(i).AggiungiDomanda(new Domanda(DomandeBufferString, risposteBufferString));
+					lettera++;	
+				}
+					
+			} 
+			else 
+			{
+				testdinamicoTest.QuizPresenti.get(i).AddParametri(punteggiopositvo, negativopositvo, "A");
+				System.out.print("Domanda: ");
+				DomandeBufferString=readScanner.nextLine();
+				System.out.print("Risposta: ");
+				risposteBufferString=readScanner.nextLine();
+				testdinamicoTest.QuizPresenti.get(i).AggiungiDomanda(new Domanda(DomandeBufferString, risposteBufferString));	
+			}	
 		}
 		
+		System.out.println("Ho finito correttamente");
+		System.out.println("Ora Domande \n \n");		
+		for (int i = 0; i < numeroDiquizDacreare; i++) 
+		{
+			if(testdinamicoTest.QuizPresenti.get(i).getTipologiaTest().contentEquals("M"))
+			{
+				System.out.println("Domanda - "+testdinamicoTest.QuizPresenti.get(i).Domande.get(0).Domanda);
+				for (int j = 1; j < testdinamicoTest.QuizPresenti.get(i).Domande.size(); j++) 
+				{
+					System.out.println(testdinamicoTest.QuizPresenti.get(i).Domande.get(j).Domanda);
+				}
+				System.out.print("Risposta  : ");	
+				risposteBufferString=readScanner.nextLine();
+				System.out.println("Risposta- "+testdinamicoTest.QuizPresenti.get(i).Domande.get(0).Riposta);
+			}
+			else 
+			{
+				System.out.println("Domanda - "+testdinamicoTest.QuizPresenti.get(i).Domande.get(0).Domanda);
+				System.out.print("Risposta  : ");	
+				risposteBufferString=readScanner.nextLine();
+				System.out.println("Risposta- "+testdinamicoTest.QuizPresenti.get(i).Domande.get(0).Riposta);
+			}
+			
+		}
+
 		
 		
 	
