2923dbfc7640dc9e1cd8d5252bd925d38c8fb6d6
Antonio Lanuto <39790759+antoniolanuto2001@users.noreply.github.com>
2021-12-23 15:34:35 +0100

    Add files via upload
    +CheckBox Mostra Password
    +InsegnanteGUI
    +Studente GUI
    +Placeholder Username

---
+++ E-LearningGUIEVO1/src/GUI/StudenteGUI.java
@@ -0,0 +1,68 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import Controller.Controller;
+
+public class StudenteGUI extends JFrame {
+
+	private JFrame frame;
+	private JPanel contentPane;
+	private Controller controller;
+
+	
+
+	/**
+	 * Create the frame.
+	 */
+	public StudenteGUI(Controller c,JFrame frameChiamante) 
+	{
+		
+		frame=this;
+		controller=c;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1080, 800);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 1064, 87);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("GUI Studente");
+		lblNewLabel.setBounds(10, 11, 149, 54);
+		panel.add(lblNewLabel);
+		
+		JButton IndietroButton = new JButton("Indietro");
+		IndietroButton.setFont(new Font("Segoe UI", Font.BOLD | Font.ITALIC, 14));
+		IndietroButton.setVerticalAlignment(SwingConstants.TOP);
+		IndietroButton.setBounds(864, 11, 149, 57);
+		panel.add(IndietroButton);
+		ImageIcon imgAccopuIcon = new ImageIcon(this.getClass().getResource("/backIcona.png"));
+		IndietroButton.setIcon(imgAccopuIcon);
+		IndietroButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				frameChiamante.setVisible(true);
+				frame.setVisible(false);
+				frame.dispose();
+			}
+		});
+	}
+
+}
