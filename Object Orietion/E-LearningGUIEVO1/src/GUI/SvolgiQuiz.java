da8b7faf4ff2d29672535c30bded18caf95ccbd3
Antonio Lanuto <39790759+antoniolanuto2001@users.noreply.github.com>
2021-12-30 23:34:04 +0100

    Add files via upload

--- Object Orietion/E-LearningGUIEVO1/src/GUI/SvolgiQuiz.java
+++ Object Orietion/E-LearningGUIEVO1/src/GUI/SvolgiQuiz.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.border.EmptyBorder;
 
 import org.eclipse.osgi.internal.debug.Debug;
@@ -28,6 +29,7 @@ import javax.swing.JTextArea;
 import javax.swing.border.LineBorder;
 import javax.swing.border.EtchedBorder;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
 
 public class SvolgiQuiz extends JFrame {
 
@@ -38,6 +40,7 @@ public class SvolgiQuiz extends JFrame {
 	public Test provaTest;
 	public JTextField MultiplaRisposta[] = new JTextField[100];
 	public int aggiornaY;
+	public int PosizioneY;
 	
 	
 	/**
@@ -51,6 +54,7 @@ public class SvolgiQuiz extends JFrame {
 	{
 		frame=this;
 		controller=c;
+		PosizioneY=20;
 		int index=controller.returnIndexListaTest(nomeTest, UtenteTest);
 		System.out.println("Ho preso il testo con index "+index);
 		try {
@@ -156,95 +160,74 @@ public class SvolgiQuiz extends JFrame {
 		testArrayList = controller.getTestArrayList(index);
 		NomeTestEffettivoLabel.setText("<html><div style='text-align: center;'>"+(String) testArrayList.get(0)+"</div></html>");
 		profTestEffettivoLabel.setText("<html><div style='text-align: center;'>"+(String) testArrayList.get(1)+"  "+testArrayList.get(2)+"</div></html>");
+		JPanel PanelDoveStannoQuiz = new JPanel();
+		PanelDoveStannoQuiz.setBackground(new Color(51, 102, 255));
+		PanelDoveStannoQuiz.setBounds(60, 154, 962, 471);
+		PanelDoveStannoQuiz.setLayout(null);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(60, 190, 962, 471);
 		
-		JPanel PanelVisualQuiz = new JPanel();
-		PanelVisualQuiz.setBounds(274, 114, 478, 457);
-		InformazioniPreliminari.add(PanelVisualQuiz);
-		PanelVisualQuiz.setLayout(null);
 		
-		JLabel QuizLabel = new JLabel("Quiz :");
-		QuizLabel.setFont(new Font("Segoe UI Light", Font.BOLD, 14));
-		QuizLabel.setBounds(10, 11, 87, 40);
-		PanelVisualQuiz.add(QuizLabel);
-		
-		JLabel NumeroQuiz = new JLabel("0");
-		NumeroQuiz.setFont(new Font("Segoe UI", Font.PLAIN, 17));
-		NumeroQuiz.setBounds(69, 10, 80, 40);
-		PanelVisualQuiz.add(NumeroQuiz);
-		
-		JLabel DomandaLabel = new JLabel("Domanda");
-		DomandaLabel.setFont(new Font("Segoe UI", Font.PLAIN, 13));
-		DomandaLabel.setBounds(10, 62, 87, 40);
-		PanelVisualQuiz.add(DomandaLabel);
-		
-		JLabel DomandaEffettivaLabel = new JLabel("Quanti Cani ha Antonella?");
-		DomandaEffettivaLabel.setBackground(Color.WHITE);
-		DomandaEffettivaLabel.setFont(new Font("Segoe UI", Font.PLAIN, 13));
-		DomandaEffettivaLabel.setVerticalAlignment(SwingConstants.TOP);
-		DomandaEffettivaLabel.setBounds(93, 73, 350, 65);
-		PanelVisualQuiz.add(DomandaEffettivaLabel);
-		
-		JLabel RispostaMultiplaLabelPrinciapale = new JLabel("Risposte");
-		RispostaMultiplaLabelPrinciapale.setFont(new Font("Segoe UI", Font.PLAIN, 13));
-		RispostaMultiplaLabelPrinciapale.setBounds(10, 147, 87, 40);
-		//PanelVisualQuiz.add(RispostaMultiplaLabelPrinciapale);
-
-		JRadioButton [] RispostaMultiplaRadioButton = new JRadioButton[20];
-		//PanelVisualQuiz.add(RispostaMultiplaRadioButton);
-		
-		JLabel[] RisposteMultipleLabel = new JLabel[20];
-		
-		//PanelVisualQuiz.add(RisposteMultipleLabel);
-		
-	
-		JLabel RispostaApertaLabel = new JLabel("Risposta");
-		RispostaApertaLabel.setBounds(10, 149, 73, 38);
-		//PanelVisualQuiz.add(RispostaApertaLabel);
-		JTextArea RispostaApertaTextArea = new JTextArea();
-		RispostaApertaTextArea.setBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(106, 90, 205), new Color(147, 112, 219)));
-		RispostaApertaTextArea.setFont(new Font("Lucida Console", Font.PLAIN, 13));
-		RispostaApertaTextArea.setToolTipText("Scrivi Qui La Risposta");
-		RispostaApertaTextArea.setLineWrap(true);
-		RispostaApertaTextArea.setBounds(93, 149, 350, 221);
-		//PanelVisualQuiz.add(RispostaApertaTextArea);
+		FormVisualQuiz[] quiz= new FormVisualQuiz[100];
 		for (int i = 0; i < provaTest.QuizPresenti.size(); i++) 
 		{
 			if (provaTest.QuizPresenti.get(i).getTipologiaTest().contentEquals("A"))
 			{
 				String s=String.valueOf(i+1);
-				NumeroQuiz.setText(s);
-				DomandaEffettivaLabel.setText(provaTest.QuizPresenti.get(i).Domande.get(0).Domanda);
-				PanelVisualQuiz.add(RispostaApertaLabel);
-				PanelVisualQuiz.add(RispostaApertaTextArea);
-				PanelVisualQuiz.repaint();
-				PanelVisualQuiz.revalidate();
+				quiz[i]=new FormVisualQuiz();
+				quiz[i].NumeroQuiz.setText(s);
+				quiz[i].DomandaEffettivaLabel.setText(provaTest.QuizPresenti.get(i).Domande.get(0).Domanda);
+				quiz[i].add(quiz[i].RispostaApertaLabel);
+				quiz[i].add(quiz[i].RispostaApertaTextArea);
+				quiz[i].repaint();
+				quiz[i].revalidate();
+				PanelDoveStannoQuiz.add(quiz[i]);
+			    PanelDoveStannoQuiz.repaint();
+			    PanelDoveStannoQuiz.revalidate();
+			    quiz[i].setLocation(233, PosizioneY);
+			    PosizioneY=PosizioneY+490;
+			    PanelDoveStannoQuiz.setPreferredSize(new Dimension(962, 500*provaTest.QuizPresenti.size()));
+			    scrollPane.setPreferredSize(new Dimension(962,500*provaTest.QuizPresenti.size()));
 			}
 			else 
 			{
 				String s=String.valueOf(i+1);
-				NumeroQuiz.setText(s);
-				DomandaEffettivaLabel.setText(provaTest.QuizPresenti.get(i).Domande.get(0).Domanda);
-				PanelVisualQuiz.add(RispostaMultiplaLabelPrinciapale);
+				quiz[i]=new FormVisualQuiz();
+				quiz[i].NumeroQuiz.setText(s);
+				quiz[i].DomandaEffettivaLabel.setText(provaTest.QuizPresenti.get(i).Domande.get(0).Domanda);
+				quiz[i].add(quiz[i].RispostaMultiplaLabelPrinciapale);
 				for (int j = 1; j < provaTest.QuizPresenti.get(i).Domande.size(); j++) 
 				{
-					RisposteMultipleLabel[j]=new JLabel();
-					RisposteMultipleLabel[j].setFont(new Font("Segoe UI", Font.PLAIN, 17));
-					RisposteMultipleLabel[j].setBounds(40, aggiornaY, 43, 58);
-					RisposteMultipleLabel[j].setText(provaTest.QuizPresenti.get(i).Domande.get(j).Domanda);
-					RispostaMultiplaRadioButton[j]=new JRadioButton();
-					RispostaMultiplaRadioButton[j].setFont(new Font("Source Code Pro", Font.PLAIN, 14));
-					RispostaMultiplaRadioButton[j].setBounds(69, aggiornaY+3, 374, 58);
-					RispostaMultiplaRadioButton[j].setText(provaTest.QuizPresenti.get(i).Domande.get(j).Riposta);
+					quiz[i].RisposteMultipleLabel[j]=new JLabel();
+					quiz[i].RisposteMultipleLabel[j].setFont(new Font("Segoe UI", Font.PLAIN, 17));
+					quiz[i].RisposteMultipleLabel[j].setBounds(40, aggiornaY, 43, 58);
+					quiz[i].RisposteMultipleLabel[j].setText(provaTest.QuizPresenti.get(i).Domande.get(j).Domanda);
+					quiz[i].RispostaMultiplaRadioButton[j]=new JRadioButton();
+					quiz[i].RispostaMultiplaRadioButton[j].setFont(new Font("Source Code Pro", Font.PLAIN, 14));
+					quiz[i].RispostaMultiplaRadioButton[j].setBounds(69, aggiornaY+3, 374, 58);
+					quiz[i].RispostaMultiplaRadioButton[j].setText(provaTest.QuizPresenti.get(i).Domande.get(j).Riposta);
 					aggiornaY=aggiornaY+40;
-					PanelVisualQuiz.add(RisposteMultipleLabel[j]);
-					PanelVisualQuiz.add(RispostaMultiplaRadioButton[j]);
-					PanelVisualQuiz.repaint();
-					PanelVisualQuiz.revalidate();
+					quiz[i].add(quiz[i].RisposteMultipleLabel[j]);
+					quiz[i].add(quiz[i].RispostaMultiplaRadioButton[j]);
+					quiz[i].repaint();
+					quiz[i].revalidate();
+					PanelDoveStannoQuiz.setPreferredSize(new Dimension(962, 500*provaTest.QuizPresenti.size()));
+				    scrollPane.setPreferredSize(new Dimension(962,500*provaTest.QuizPresenti.size()));
+				    PanelDoveStannoQuiz.add(quiz[i]);
+				    quiz[i].setLocation(233, PosizioneY);
+				    PanelDoveStannoQuiz.repaint();
+				    PanelDoveStannoQuiz.revalidate();
 				}
+				  PosizioneY=PosizioneY+490;
+				  aggiornaY=191;
 			}
-		}	
-		
-		
+		}
+		InformazioniPreliminari.add(scrollPane);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+	    scrollPane.setViewportView(PanelDoveStannoQuiz);
+	    InformazioniPreliminari.add(scrollPane);
+		//InformazioniPreliminari.add(quiz);
 		
 	}
 }
